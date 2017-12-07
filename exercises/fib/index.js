69ce6bc3e27dea5b8994615af98019f74e0319de
Arun Reddy <arunreddy.kayathi@gmail.com>
2017-12-06 23:23:07 -0600

    Fibonacci with iterative, recursive and memoization solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,53 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Iterative Solution
+
+/*function fib(n) {
+    const fibArray=[0,1]
+    
+    for(let i=2;i<=n;i++){
+        const a =fibArray[i-1];
+        const b=fibArray[i-2];
+        fibArray.push(a+b);
+    }
+    return fibArray[n];
+
+}*/
+
+//Recursive solution(O(2^n) time)
+
+/*function fib(n){
+
+    if(n<2){
+        return n;
+    }
+    return fib(n-1)+fib(n-2)
+}*/
+
+//Memoization Solution
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        } else {
+            let result = fn.apply(this, args);
+            cache[args] = result;
+            return result;
+        }
+
+    }
+}
+
+function fib(n) {
+
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2)
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
